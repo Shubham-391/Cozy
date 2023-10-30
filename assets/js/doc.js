039688fbd49b433afd5c153524802a1e97a6fcc9
Shubham-391 <subhamy1234@gmail.com>
2023-10-30 14:45:22 +0530

    preloader

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -1,14 +1,19 @@
 let hide = document.querySelector(".hide");
+// preloader
+const preloader = document.getElementById("preloader")
+setTimeout(() => {
+    document.getElementById("preloder").classList.add("d-none");
+}, 2000)
 
 function phase(evt, Id) {
     let box3 = document.querySelectorAll(".box3");
-    document.getElementById(Id).classList.remove("d_none");
-    hide.classList.add("d_none");
+    document.getElementById(Id).classList.remove("d-none");
+    hide.classList.add("d-none");
 }
 
 function show(evt, Id) {
     let box3 = document.querySelectorAll(".box3");
-    document.getElementById(Id).classList.add("d_none");
-    hide.classList.remove("d_none");
+    document.getElementById(Id).classList.add("d-none");
+    hide.classList.remove("d-none");
 }
 
